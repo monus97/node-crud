45bd5707c15ff12140c2c59551dec876afccd478
monus97 <monus3871@gmail.com>
2023-06-12 11:48:11 +0530

    working on

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ const productRouter = require("./routes/productRouter");
 const cors = require("cors");
 
 require("./models/config");
-
+app.use(cors());
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.get("/cors", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.send({ msg: "This has CORS enabled 🎈" });
-});
+
+
 app.use("/", productRouter);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port : ${process.env.PORT}`);
